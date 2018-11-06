8863a2b793f0436a6f5112d0dea2c85e569ba563
manobray <manobray@gmail.com>
2018-11-06 00:05:27 -0200

    Projeto asp net core

---
+++ TestDocker/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestDocker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        // GET: api/Hello
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from .NET Core";
+        }
+    }
+}
